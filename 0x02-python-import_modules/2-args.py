289f00581a9796c56da107162bba128f7c834008
ajoued <ajoueds@gmail.com>
2023-10-07 15:35:19 +0100

    Task 2 Fixed pycodestyle

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,5 +9,5 @@ if __name__ == '__main__':
     elif (leng > 1):
         print("{} arguments:".format(leng))
     if (leng > 0):
-        for i in range (1,leng + 1):
+        for i in range(1, leng + 1):
             print("{}: {}".format(i, argv[i]))
