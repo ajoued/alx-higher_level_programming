8da566984c78376575ef5bb561f84ec5771f0f56
ajoued <ajoueds@gmail.com>
2023-10-08 22:15:13 +0100

    Task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    if (type(my_list) == int):
-        for i in my_list:
-            print("{}".format(i))
+    for i in my_list:
+        print("{}".format(i))
